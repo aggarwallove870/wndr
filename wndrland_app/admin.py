176372ce5a66791f9a47a5241520e0e0a994841b
rishi0478 <rishimehra0478@gamil.com>
2022-10-03 13:07:06 +0530

    remove subscribe from adminpanel

--- wndrland_app/admin.py
+++ wndrland_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Subscriber_newsletter)
+# admin.site.register(Subscriber_newsletter)
 
 @admin.register(contact_us)
 class contact_us_users(admin.ModelAdmin):
